fc7f579d174f3b13464b38456f018baca1e828d3
Nicolás <bul86846@educastur.es>
2025-03-31 19:47:48 +0200

    31/03/2025

--- src/fp/dam/psp/servidorcontactos/cliente/Main.java
+++ src/fp/dam/psp/servidorcontactos/cliente/Main.java
@@ -1,13 +1,13 @@
 package fp.dam.psp.servidorcontactos.cliente;
 
+import javax.crypto.*;
 import java.io.ByteArrayInputStream;
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
 import java.net.Socket;
-import java.net.UnknownHostException;
+import java.security.GeneralSecurityException;
 import java.security.PublicKey;
-import java.security.cert.CertificateException;
 import java.security.cert.CertificateFactory;
 import java.security.cert.X509Certificate;
 import java.util.Base64;
@@ -19,13 +19,29 @@ public class Main {
             Base64.Decoder decoder = Base64.getDecoder();
             DataInputStream in = new DataInputStream(socket.getInputStream());
             DataOutputStream out = new DataOutputStream(socket.getOutputStream());
+
+            //Leer certificado enviado por el servidor
             String b64Certificate = in.readUTF();
             byte[] certificateBytes = decoder.decode(b64Certificate);
             CertificateFactory cf = CertificateFactory.getInstance("X509");
             X509Certificate certificate = (X509Certificate) cf.generateCertificate(new ByteArrayInputStream(certificateBytes));
             PublicKey publicKey = certificate.getPublicKey();
-            System.out.println(certificate.getSubjectX500Principal().getName());
-        } catch (IOException | CertificateException e) {
+            //System.out.println(certificate.getSubjectX500Principal().getName());
+
+            //Crear clave secreta, cifrarla y enviarla al servidor
+            KeyGenerator kg = KeyGenerator.getInstance("AES");
+            kg.init(256);
+            SecretKey key = kg.generateKey();
+            Cipher cipher = Cipher.getInstance(publicKey.getAlgorithm()+"/ECB/PKCS5Padding");
+            byte[] encriptedKey = cipher.doFinal(key.getEncoded());
+            Base64.Encoder encoder = Base64.getEncoder();
+            out.writeUTF(encoder.encodeToString(encriptedKey));
+
+            //Enviar al servidor el algoritmo de cifrado simetrico
+            out.writeUTF("AES/ECB/PKCS5Padding");
+
+
+        } catch (IOException | GeneralSecurityException e) {
             throw new RuntimeException(e);
         }
     }
